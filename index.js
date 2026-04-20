857065fc69bc6970416e51c4124cd78c9f2f7ee7
RadithSandeepa <radithsandeepa500@gmail.com>
2026-04-20 14:29:22 +0530

    feat: Add CORS middleware and update dependencies

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 import express from 'express';
+import cors from 'cors';
 import userRoutes from './routes/userRoutes.js';
 import seedAdmin from './seeders/adminSeeder.js';
 import { swaggerDocs } from "./docs/swagger.js";
 
 const app = express();
-swaggerDocs(app);
+app.use(cors({
+  origin: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
+swaggerDocs(app);
 
 app.use('/users', userRoutes);
 
 // Start server
 app.listen(PORT, async () => {
   console.log(`User Service running on port ${PORT}`);
-  await seedAdmin(); // seed admin at startup
+  // await seedAdmin(); // seed admin at startup
 })
